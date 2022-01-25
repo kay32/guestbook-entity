2084e1ab7ccb006a047939a1eb8fd3a84ad54826
Kykot Oleksandr <k1.edu@outlook.com>
2022-01-25 17:16:49 +0200

    Added AJAX validation

--- web/modules/custom/guestbook_entity/src/Form/FeedbackForm.php
+++ web/modules/custom/guestbook_entity/src/Form/FeedbackForm.php
@@ -2,8 +2,11 @@
 
 namespace Drupal\guestbook_entity\Form;
 
+use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Ajax\RedirectCommand;
 use Drupal\Core\Entity\ContentEntityForm;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
 
 /**
  * Form controller for the feedback entity edit forms.
@@ -23,7 +26,10 @@ class FeedbackForm extends ContentEntityForm {
     $link = $entity->toLink($this->t('View'))->toRenderable();
 
     $message_arguments = ['%label' => $this->entity->label()];
-    $logger_arguments = $message_arguments + ['link' => render($link)];
+    $logger_arguments =
+      $message_arguments + [
+        'link' => \Drupal::service('renderer')->render($link),
+      ];
 
     if ($result == SAVED_NEW) {
       $this->messenger()
@@ -39,6 +45,36 @@ class FeedbackForm extends ContentEntityForm {
     }
 
     $form_state->setRedirect('entity.kay_feedback.canonical', ['kay_feedback' => $entity->id()]);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state): array {
+    $form = parent::buildForm($form, $form_state);
+    $currentRoute = \Drupal::routeMatch()->getRouteName();
+    if ($currentRoute == 'guestbook_entity.main') {
+      $form['#prefix'] = '<div id="kay-feedback-form-wrapper">';
+      $form['#suffix'] = '</div>';
+      $form['actions']['submit']['#ajax'] = [
+        'callback' => '::submitAjaxCallback',
+        'wrapper' => 'kay-feedback-form-wrapper',
+      ];
+    }
+    return $form;
+  }
+
+  /**
+   * AJAX form submit handler.
+   */
+  public function submitAjaxCallback(array &$form, FormStateInterface $form_state) {
+    if ($form_state->getErrors()) {
+      return $form;
+    }
+    $redirectUrl = Url::fromRoute('guestbook_entity.main')->toString();
+    $response = new AjaxResponse();
+    $response->addCommand(new RedirectCommand($redirectUrl));
+    return $response;
   }
 
 }
